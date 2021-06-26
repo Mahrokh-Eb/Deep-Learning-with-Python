842f7f52e0966c54ecfc950e35d6fe497020c975
Mahrokh Ebrahimi <mahrokhh.ebrahimi@gmail.com>
2021-06-25 22:32:24 -0700

    Update spilit.py

--- kaggle/spilit.py
+++ kaggle/spilit.py
@@ -4,10 +4,10 @@ import shutil
 import os
 import sys
 
-labels = pd.read_csv(r'/content/drive/MyDrive/dogVsCat/kaggle/sampleSubmission.csv')
+labels = pd.read_csv(r'/content/drive/MyDrive/dogVsCat/kaggle/sampleSubmission.csv') # give the address of your csv file
 
-train_dir =r'/content/drive/MyDrive/dogVsCat/kaggle/train'
-DR = r"/content/drive/MyDrive/dogVsCat/kaggle/D"
+train_dir =r'/content/drive/MyDrive/dogVsCat/kaggle/train' #the address od train set folder
+DR = r"/content/drive/MyDrive/dogVsCat/kaggle/DR" #the DR folder is maded by me outside of the train folder
 if not os.path.exists(DR):
     os.mkdir(DR)
 
@@ -15,7 +15,7 @@ for filename, class_name in labels.values:
     # Create subdirectory with `class_name`
     if not os.path.exists(DR + str(class_name)):
         os.mkdir(DR + str(class_name))
-    src_path = str(train_dir) + '/'+ str(filename) + str('.jpg')
+    src_path = train_dir + '/'+'cat.'+ str(filename) + '.jpg' #be carefull with the image name. check it in train folder
     dst_path = str(DR) + str(class_name) + '/' + str(filename) + str('.jpg')
     try:
         shutil.copy(src_path, dst_path)
